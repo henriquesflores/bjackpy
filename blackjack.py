c1e1bcdfec0db4f8b36f82d75f7c8b1f2d5c9ff3
Henrique Flores <henriquemfl@hotmail.com>
2022-05-11 12:06:56 +0200

    Can now evaluate hand value with appropriate choice for A.

--- blackjack.py
+++ blackjack.py
@@ -1,33 +1,56 @@
 import sys
 import random
 
-from typing import List, Dict
+from typing import List, Dict, Tuple
 
-# NOTE (Henrique): [spades, diamonds, clubs, hearts]
-SUITS = ["♠", "♦", "♣", "♥"]
-#CARDS = {"A": 1, "2": 2, "3": 3, "4": 4, "5": 5, "6": 6, "7": 7, "8": 8, "9": 9, "10": 10, "J": 11, "Q": 12, "K": 13} 
-CARDS = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
-LEN_DECK = 52
+def make_deck(N: int) -> Dict[str, int]:
+    # NOTE (Henrique): [spades, diamonds, clubs, hearts]
+    SUITS = ["♠", "♦", "♣", "♥"]
+    CARDS = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
 
-def make_deck() -> Dict[str, int]:
     deck = []
     for suit in SUITS:
         for card in CARDS:
            deck.append(card + suit)
 
+    LEN_DECK = 52
+    assert len(deck) == LEN_DECK
+
+    deck *= N
     return deck
 
-def shuffle(N_decks: int) -> List[int]:
-    return random.sample(range(52), 4 * N_decks)
+def give_hand(deck: List[str], size: int) -> List[int]:
+    N = len(deck)
+    hand_indices = random.sample(range(N), size)
+    return [deck[index] for index in hand_indices]
+
+def is_ace_in_hand(hand: List[str]) -> bool:
+    return any(["A" in x for x in hand])
+
+def eval_hand(hand: List[str]) -> int: 
+    value_table_A = {"A":  1, "2": 2, "3": 3, "4": 4, "5": 5, "6": 6, "7": 7, "8": 8, "9": 9, "10": 10, "J": 10, "Q": 10, "K": 10}
+
+    hand_value = 0 
+    for card in hand: 
+        value = card[0]
+        hand_value += value_table_A[value]
+    
+    if hand_value < 12 and is_ace_in_hand(hand):
+        hand_value += 10
+
+    return hand_value 
 
 def main(argv: List[str]) -> int: 
 
-    random.seed(42)
+    random.seed(2)
 
-    deck = make_deck()
-    order = shuffle(1)
-    hand = [deck[card] for card in order]
+    deck = make_deck(2)
+    hand = give_hand(deck, 2)
+
+    hand = ["A♦", "J♣", "3♦"]
     print(hand)
+    vb = eval_hand(hand)
+    print(vb)
 
     return 0
 
